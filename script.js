c71dbc76b54caaa79427cc8b8a86343369a9f23c
Haal20 <69975021+Haal20@users.noreply.github.com>
2020-09-07 10:02:04 +0200

    skapar tomma funktioner som kommer behövas

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(typeof "Hej världen!");
+//console.log för att se så att script-fil är kopplat med index-fil
+console.log("Hej världen!");
+
+//Hämtar mina object från DOM
+let key = document.getElementById("iKey");
+let value = document.getElementById("iValue");
+let button = document.getElementById("iButtton");
+
+console.log();
+
+function name(params) {
+  //skriv ut besökarens namn i nytt <div> element
+}
+
+function name(params) {
+  //Spara besökarens namn i lokalStorage
+}
+
+function name(params) {
+  //radera knapp som raderar sparat namn från lokalStorage
+}
